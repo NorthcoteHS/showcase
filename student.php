3cc5eb27463649e0280155f6e43c1f8993300505
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2019-06-17 21:41:23 +1000

    Change titles

--- student.php
+++ student.php
@@ -51,7 +51,7 @@ $studentName = $student['firstName'] . ' ' . $student['lastName'];
 
 <html>
   <head>
-    <title>Menu</title>
+    <title>Festival of Thinking - Student</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta charset="UTF-8">
 
